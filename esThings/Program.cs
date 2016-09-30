3d18df72dfa7cae471763ae959a0c89607fd53bd
Eric Sondergard <flamtap@users.noreply.github.com>
2016-09-29 23:51:36 -0600

    Simple device key creation example

--- esThings/Program.cs
+++ esThings/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace CreateDeviceIdentity
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static RegistryManager RegistryManager;
+        public static string ConnectionString =
+            "HostName=esThings.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=fOqVTq6qlMj5ox7w9AJTItDxNzxKTqL8QSjE78xdAlg=";
+
+        public static void Main(string[] args)
         {
+            RegistryManager = RegistryManager.CreateFromConnectionString(ConnectionString);
+
+            AddDeviceAsync().Wait();
+            
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "esDevice";
+
+            Device device;
+
+            try
+            {
+                device = await RegistryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await RegistryManager.GetDeviceAsync(deviceId);
+            }
+
+            Console.WriteLine($"Generated device key: {device.Authentication.SymmetricKey.PrimaryKey}");
         }
     }
 }
