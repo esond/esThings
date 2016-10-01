d5f13f2826e9dcfdadc72374690c8b5002d2e442
Eric Sondergard <flamtap@users.noreply.github.com>
2016-10-01 17:44:38 -0600

    Configuration View, controller, and removing Application Insights
    Just plain don't need MS AI.

---
+++ esThings/Controllers/ConfigurationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace esThings.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        // GET: Configuration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
