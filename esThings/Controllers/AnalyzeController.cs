bf3ab851ab5d796df4b137ce6c94a3fbb95ee8e0
Eric Sondergard <flamtap@users.noreply.github.com>
2016-10-01 17:46:55 -0600

    Analyze view and controller

---
+++ esThings/Controllers/AnalyzeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace esThings.Controllers
+{
+    public class AnalyzeController : Controller
+    {
+        // GET: Analyze
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
