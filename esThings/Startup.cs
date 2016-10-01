7fab17e8ae8d7a5a935b5949d434cdb60f31946b
Eric Sondergard <flamtap@users.noreply.github.com>
2016-10-01 17:01:49 -0600

    Default MVC project initial commit
    This default project is very bloated, but we'll just check everything in
    for now and remove what we need to later.

---
+++ esThings/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(esThings.Startup))]
+namespace esThings
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
