4e7f83c7e04ce662cd769a36b70641a4648b8396
Eric Sondergard <flamtap@users.noreply.github.com>
2016-10-02 16:33:59 -0600

    esThings.Devices.Simulation project
    This project will be responsible for the administration and simulation
    of device behavior.

---
+++ esThings.Devices.Simulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esThings.Devices.Simulation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
