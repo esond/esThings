c787a58e50bb8f28591ff9764d6cde835881a325
Eric Sondergard <flamtap@users.noreply.github.com>
2016-10-01 18:32:03 -0600

    esThings.Devices project
    Project containing classes for modelling device behavior

---
+++ esThings.Devices/GarbageCanMonitor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace esThings.Devices
+{
+    public class GarbageCanMonitor
+    {
+        public GarbageCanMonitor(int id, string deviceKey)
+        {
+            Id = id;
+            DeviceKey = deviceKey;
+        }
+
+        public int Id { get; set; }
+
+        public string DeviceKey { get; set; }
+
+        public int Fullness { get; set; }
+
+        public int MessageIntervalSeconds { get; set; }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SendUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
